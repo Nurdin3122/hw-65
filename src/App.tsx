1303477b010f02902ea112c4dbb41516528930b6
Muhtarov Nurdin <nurdinmuhtarovv@gmail.com>
2024-07-05 13:10:28 +0600

    init app,скачал: axios ,bootstrap ,react-router-dom.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import {Routes} from "react-router-dom";
+
+const App = () => (
+    <>
+       <p>hello</p>
+        <Routes>
+
+        </Routes>
+    </>
+);
+
+export default App
